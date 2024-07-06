97077113d478ef42469f2efda12f0765f9bca591
Hridya Agrawal <hridayahoney@gmail.com>
2024-07-06 19:57:38 +0000

    twitter widget improved

--- src/containers/twitter-embed/twitter.js
+++ src/containers/twitter-embed/twitter.js
@@ -1,54 +1,56 @@
-import React, {Suspense, useContext} from "react";
+import React, { Suspense, useContext, useEffect } from "react";
 import "./twitter.scss";
 import Loading from "../loading/Loading";
-import {TwitterTimelineEmbed} from "react-twitter-embed";
-import {twitterDetails} from "../../portfolio";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import { twitterDetails } from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 const renderLoader = () => <Loading />;
-const cantDisplayError =
-  "<div className='centerContent'><h2>Can't load? Check privacy protection settings</h2></div>";
+const cantDisplayError = `
+  <div class='centerContent error'>
+    <h2>Can't load? Check privacy protection settings</h2>
+  </div>`;
 
-function timeOut() {
-  setTimeout(function () {
-    if (!document.getElementById("twitter").innerHTML.includes("iframe")) {
-      document.getElementById("twitter").innerHTML = cantDisplayError;
-    }
-  }, 10000);
-}
-var widthScreen = window.screen.width;
+function Twitter() {
+  const { isDark } = useContext(StyleContext);
 
-export default function Twitter() {
-  const {isDark} = useContext(StyleContext);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (document.getElementById("twitter") && !document.getElementById("twitter").innerHTML.includes("iframe")) {
+        document.getElementById("twitter").innerHTML = cantDisplayError;
+      }
+    }, 10000);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   if (!twitterDetails.display) {
     return null;
   }
   if (!twitterDetails.userName) {
     console.error("Twitter username for twitter section is missing");
-  }
-  if (twitterDetails.userName) {
-    return (
-      <Suspense fallback={renderLoader()}>
-        <div className="tw-main-div" id="twitter">
-          <div className="centerContent">
-            <TwitterTimelineEmbed
-              sourceType="profile"
-              screenName={twitterDetails.userName}
-              options={{height: 400, width: {widthScreen}}}
-              placeholder={renderLoader()}
-              autoHeight={false}
-              borderColor="#fff"
-              key={isDark ? "1" : "2"}
-              theme={isDark ? "dark" : "light"}
-              noFooter={true}
-              onload={timeOut()}
-            />
-          </div>
-        </div>
-      </Suspense>
-    );
-  } else {
     return null;
   }
+
+  return (
+    <Suspense fallback={renderLoader()}>
+      <div className="tw-main-div" id="twitter">
+        <div className="centerContent">
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName={twitterDetails.userName}
+            options={{ height: 400, width: window.screen.width }}
+            placeholder={renderLoader()}
+            autoHeight={false}
+            borderColor="#fff"
+            key={isDark ? "1" : "2"}
+            theme={isDark ? "dark" : "light"}
+            noFooter={true}
+          />
+        </div>
+      </div>
+    </Suspense>
+  );
 }
+
+export default Twitter;
